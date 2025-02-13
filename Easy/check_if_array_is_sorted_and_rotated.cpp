325a5213ab75aa5067800c516f0bd7b69383565b
mr-anonymous-10 <mr.anonymous.hacker10@gmail.com>
2025-02-13 17:57:20 +0530

    Update check_if_array_is_sorted_and_rotated.cpp

--- Easy/check_if_array_is_sorted_and_rotated.cpp
+++ Easy/check_if_array_is_sorted_and_rotated.cpp
@@ -4,7 +4,7 @@ public:
     bool check(vector<int>& nums) {
          int count=0; 
          int n=nums.size();
-
+        // this is for rotated
          for(int i=1; i<n; i++){
             if(nums[i-1] > nums[i])
                 count++;
